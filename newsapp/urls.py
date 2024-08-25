41988df96195008881d64988bb7c7e42a5023132
dan.toropov <dan.toropov@gmail.com>
2024-08-25 20:51:48 +0300

    create site .

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from newsapp.views import index
+from newsapp.apps import NewsappConfig
+from newsapp.views import NewsLetterListView, NewsLetterDetailView, change_status
 
-app_name = 'newsapp'
+app_name = NewsappConfig.name
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', NewsLetterListView.as_view(), name='home'),
+    path('newsletter_view/<int:pk>', NewsLetterDetailView.as_view(), name='newsletter_view'),
+    path('status/<int:pk>', change_status, name='status'),
+
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
 
-def index(request):
-    return render(request, 'newsapp/index.html')
+
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+
+from newsapp.models import NewsLetter
+
+
+class NewsLetterListView(ListView):
+    model = NewsLetter
+    template_name = 'newsapp/index.html'
+    extra_context = {
+        'title': 'Рассылки'
+    }
+
+class NewsLetterDetailView(DetailView):
+    model = NewsLetter
+
+
+def change_status(request, pk):
+    newsletter_item = get_object_or_404(NewsLetter, pk=pk)
+    if newsletter_item.status == 'ON':
+        newsletter_item.status = 'OFF'
+    elif newsletter_item.status == 'OFF':
+        newsletter_item.status = 'ON'
+    newsletter_item.save()
+
+    return redirect(reverse('newsapp:home'))
+
+
+
+
+
