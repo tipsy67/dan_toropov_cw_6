4d429f090e5edbb91de57099ef31102e0b0befb9
dan.toropov <dan.toropov@gmail.com>
2024-09-18 21:53:56 +0300

    add app blog from hw

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     'newsapp',
     'django_apscheduler',
+    'blog',
+
 ]
 
 MIDDLEWARE = [
--- newsapp/urls.py
+++ newsapp/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from newsapp.apps import NewsappConfig
 from newsapp.views import NewsLetterListView, NewsLetterDetailView, change_status, ClientListView, MessageListView, \
     ClientUpdateView, MessageUpdateView, ClientCreateView, NewsLetterCreateView, MessageCreateView, \
-    NewsLetterUpdateView, MessageDeleteView, NewsLetterDeleteView, ClientDeleteView
+    NewsLetterUpdateView, MessageDeleteView, NewsLetterDeleteView, ClientDeleteView, mainpage
 
 app_name = NewsappConfig.name
 
 urlpatterns = [
-    path('', NewsLetterListView.as_view(), name='newsletter_list'),
+    path('', mainpage, name='home'),
+    path('newsletter_list', NewsLetterListView.as_view(), name='newsletter_list'),
     path('newsletter_view/<int:pk>', NewsLetterDetailView.as_view(), name='newsletter_view'),
     path('newsletter_edit/<int:pk>', NewsLetterUpdateView.as_view(), name='newsletter_edit'),
     path('newsletter_delete/<int:pk>', NewsLetterDeleteView.as_view(), name='newsletter_delete'),
